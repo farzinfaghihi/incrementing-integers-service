8d2c314b6c21b30f785b6f4631d8b3d46f08bbc9
Farzin Faghihi <ffaghihi@me.com>
2019-05-11 10:52:23 -0700

    Added base Spring Boot project

---
+++ thinkific/src/main/java/com/farzinfaghihi/thinkific/ThinkificApplication.java
@@ -0,0 +1,13 @@
+package com.farzinfaghihi.thinkific;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkificApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinkificApplication.class, args);
+	}
+
+}
---
+++ thinkific/src/test/java/com/farzinfaghihi/thinkific/ThinkificApplicationTests.java
@@ -0,0 +1,16 @@
+package com.farzinfaghihi.thinkific;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThinkificApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
